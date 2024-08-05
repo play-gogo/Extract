5b0b65b097c20ded62ed0720dd14bb8d650ba454
sooj1n <sujin19371@gmail.com>
2024-08-05 12:09:27 +0900

    get data

--- src/extrct/mov.py
+++ src/extrct/mov.py
@@ -2,20 +2,47 @@ import pandas as pd
 import requests
 import os
 import json
-
+from pprint import pprint
 def get_key():
     """영화진흥위원회 가입 및 API 키 생성 후 환경변수 선언 필요"""
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def movie_url(dt="20120101", url_param = {}):
+def gen_url(dt="20200101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
+    #url = f"{base_url}?key={key}&targetDt={dt}"
+    #for key, value in url_param.items():
+    #    url = url + f"&{key}={value}"
+    
+    #url_param = {
+    #        'multiMovieYn' : 'Y',
+    #        'multiMovieYn' : 'N',
+    #        'repNationCd' : 'K',
+    #        'repNationCd' : 'F',
+    #        'wideAreaCd' : 
+    #}
     url = f"{base_url}?key={key}&targetDt={dt}"
-    for key, value in url_param.items():
-        #url = url + "&multiMovieYn=N"
-        url = url + f"&{key}={value}"
 
     return url
 
+def req(load_dt="20200101"):
+    url = gen_url(load_dt)
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+    pprint(data)
+    return code, data
 
+def req2list(load_dt='20200101') -> list:
+    _,data = req(load_dt)
+    l=data['boxOfficeResult']['dailyBoxOfficeList']
+    return l
+
+def list2df(load_dt='20120101'):
+    l = req2list(load_dt)
+    df = pd.DataFrame(l)
+    print(df)
+    return df
+
+list2df()
